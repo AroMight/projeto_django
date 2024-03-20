ef7085ebaff5a638a3b309c9439355c0a69edd5a
Denilson Silva <fcodenilson@gmail.com>
2024-03-20 19:37:47 -0300

    Entendendo arquivos estáticos

--- site_receitas/settings.py
+++ site_receitas/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'site_receitas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'global_templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Adicionar pasta para static files
+STATICFILES_DIRS = [
+    BASE_DIR / 'global_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
