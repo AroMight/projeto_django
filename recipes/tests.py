027ee48f65017f22c955d24f1d56491e82ec89d4
Denilson Silva <fcodenilson@gmail.com>
2024-03-29 09:12:21 -0300

    config tests and some small changes

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class RecipeURLSTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        print('Olá mundo')
+        assert 1 == 2, 'Um não é igual a dois'
--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,13 @@ from utils.recipes.factory import *
 from .models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+            is_published=True
+            ).order_by('-id')
+
     return render(request,'recipes/pages/home.html', context={
         'recipes': recipes,
-    })
+        })
   
 def category(request, category_id):
     recipes = get_list_or_404(
