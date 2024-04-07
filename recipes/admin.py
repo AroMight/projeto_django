b94b6db5204e0786cd108cc1cb23c0a3172719a3
Denilson Silva <fcodenilson@gmail.com>
2024-04-07 08:23:31 -0300

    finished | conf admin_login

--- recipes/admin.py
+++ recipes/admin.py
@@ -4,12 +4,37 @@ from .models import *
 
 #OUTRO MÉTODO
 class RecipeAdmin(admin.ModelAdmin):
-    ...
-
-@admin.register(Recipe)
+    fieldsets = [
+        ('Recipes Details', {'fields':['title', 'description','slug','cover','category']}),
+        ('Preparation Details', {'fields':[
+            'preparation_time',
+            'preparation_time_unit',
+            'servings',
+            'servings_unit',
+            'preparation_steps',
+            ]}),
+        ('Advanced', {'fields':['preparation_step_is_html','is_published','author'],'classes':['collapse']})
+        ]
+    list_display = [
+        'title',
+        'updated_at',
+        'created_at',
+        'preparation_step_is_html',
+        'is_published'
+        ]
+    list_filter=[
+        'is_published',
+        'created_at',
+        'updated_at',
+        'author'
+        ]
+    search_fields = [
+        'title',
+        ]
 
 #MÉTODO MAIS FÁCIL
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
+admin.site.register(Recipe,RecipeAdmin)
 admin.site.register(Category, CategoryAdmin)
