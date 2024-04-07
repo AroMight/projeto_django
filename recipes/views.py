613a9c697bb49ec8571b36043786c9106bc1e596
Denilson Silva <fcodenilson@gmail.com>
2024-04-07 12:03:16 -0300

    adding pagination

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,20 @@
 from .models import Recipe
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import Http404
-from utils.recipes.factory import *
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 def home(request):
     recipes = Recipe.objects.filter(
             is_published=True
             ).order_by('-id')
+    
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 2)
+    page_obj = paginator.get_page(current_page)
 
     return render(request,'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
         })
 
 def recipe(request, id, slug):
@@ -41,9 +45,11 @@ def search(request):
         raise Http404()
     
     recipes = Recipe.objects.filter(
-        Q(Q(title__icontains=search_term) |
-        Q(description__icontains=search_term) |
-        Q(category__name__icontains=search_term)),
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term) |
+            Q(category__name__icontains=search_term)
+        ),
         is_published=True,
         ).order_by('-id')
 
---
+++ utils/pagination.py
@@ -0,0 +1,37 @@
+import math
+
+
+def make_pagination_range(
+    page_range,
+    qty_pages,
+    current_page,
+    ):
+
+    middle_range = math.ceil(qty_pages / 2)
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+    stop_range_offset = stop_range - len(page_range)
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += start_range_offset
+        
+    if stop_range_offset > 0:
+        start_range -= stop_range_offset
+        
+    pagination = page_range[start_range:stop_range]
+
+    return {
+        'pagination':pagination,
+        'page_range':page_range,
+        'qty_pages':qty_pages,
+        'current_page':current_page,
+        'total_pages':len(page_range),
+        'start_range':start_range,
+        'stop_range':stop_range,
+        'first_page_out_of_rnage': current_page > middle_range,
+        'last_page_out_of_range': stop_range < len(page_range),
+    }
+    
