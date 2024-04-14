b07e98780ab6dcbb176b6c55143a51a591faac82
Denilson Silva <fcodenilson@gmail.com>
2024-04-14 12:10:25 -0300

    testings

--- authors/forms.py
+++ authors/forms.py
@@ -8,10 +8,16 @@ class RegisterForm(forms.ModelForm):
         fields = [
             'username',
             'first_name',
-            'last_name', 'email',
+            'last_name',
+            'email',
             'password',
             'email',
             ] #ou '__all__'
-        widgets = {
-            'password': forms.PasswordInput()
-        }
+        # widgets = {
+        #     'password': forms.PasswordInput()
+        # }
+
+class RegisterRecipe(forms.Form):
+    recipe_title = forms.CharField(label='Nome da Receita', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Nome da Receita'}))
+    preparation_time = forms.IntegerField(label='Tempo de Preparo')
+    recipe_img = forms.ImageField(label='Imagem da Receita')
