cd9e0501170999824e7117c73978a822a9113fa0
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-07-24 09:32:05 -0300

    small changes in url.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,19 +4,52 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name='home'),
-    path('recipes/api/v1', views.RecipeListViewHomeApi.as_view(),
-         name='recipes_api_v1'),
-    path('recipe/search/', views.RecipeListViewSearch.as_view(), name='search'),
-    path('recipe/<int:id>/<slug:slug>/',
-         views.RecipeDetail.as_view(), name='recipe'),
-    path('recipes/api/v1/<int:id>/<slug:slug>/',
-         views.RecipeDetailApi.as_view(), name='recipes_api_v1_detail'),
-    path('recipe/category/<int:category_id>/',
-         views.RecipeListViewCategory.as_view(), name='category'),
-    path('recipes/api/v2/', views.recipe_api_list, name='recipe_api_v2'),
-    path('recipes/api/v2/<int:id>', views.recipe_api_detail,
-         name='recipe_api_v2_detail'),
-    path('recipes/api/v2/category/<int:pk>', views.recipe_api_category,
-         name='recipe_api_v2_category'),
+     path(
+          '',
+          views.RecipeListViewHome.as_view(),
+          name='home'
+          ),
+          
+     path(
+          'recipes/api/v1',
+          views.RecipeListViewHomeApi.as_view(),
+          name='recipes_api_v1',
+          ),
+
+     path(
+          'recipe/search/',
+          views.RecipeListViewSearch.as_view(),
+          name='search',
+          ),
+
+     path(
+          'recipe/<int:id>/<slug:slug>/',
+          views.RecipeDetail.as_view(),
+          name='recipe',
+          ),
+
+     path(
+          'recipe/category/<int:category_id>/',
+          views.RecipeListViewCategory.as_view(),
+          name='category',
+          ),
+
+     #API Urls
+     path(
+          'recipes/api/v2/',
+          views.recipe_api_list,
+          name='recipe_api_v2',
+          ),
+
+     path(
+          'recipes/api/v2/<int:id>',
+          views.recipe_api_detail,
+          name='recipe_api_v2_detail',
+          ),
+
+     path(
+          'recipes/api/v2/category/<int:pk>',
+          views.recipe_api_category,
+          name='recipe_api_v2_category',
+          ),
 ]
