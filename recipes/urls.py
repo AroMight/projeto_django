9bb0a7a7e4ae600190fe127cdfebcba89ecd731a
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-08-01 19:17:19 -0300

    chore: Update recipes/urls.py to include API v2 router

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from rest_framework.routers import SimpleRouter
+
 
 app_name = 'recipes'
+
+recipe_api_v2_router = SimpleRouter(
+    trailing_slash=True
+)
+
+recipe_api_v2_router.register(
+    r'',
+    views.RecipeAPIv2ViewSet,
+    basename='recipe_api_v2'
+)
 
 urlpatterns = [
     path(
@@ -35,28 +47,31 @@ urlpatterns = [
     ),
 
     # API Urls
-    path(
-        'recipes/api/v2/',
-        views.RecipeAPIv2ViewSet.as_view({
-            "get": "list",
-            "post": "create",
-        }),
-        name='recipe_api_v2',
-    ),
+    # path(
+    #     'recipes/api/v2/',
+    #     views.RecipeAPIv2ViewSet.as_view({
+    #         "get": "list",
+    #         "post": "create",
+    #     }),
+    #     name='recipe_api_v2',
+    # ),
 
-    path(
-        'recipes/api/v2/<int:id>',
-        views.RecipeAPIv2ViewSet.as_view({
-            "get": "retrieve",
-            "patch": "partial_update",
-            "delete": "destroy",
-        }),
-        name='recipe_api_v2_detail',
-    ),
+    # path(
+    #     'recipes/api/v2/<int:id>',
+    #     views.RecipeAPIv2ViewSet.as_view({
+    #         "get": "retrieve",
+    #         "patch": "partial_update",
+    #         "delete": "destroy",
+    #     }),
+    #     name='recipe_api_v2_detail',
+    # ),
 
     path(
         'recipes/api/v2/category/<int:pk>',
         views.recipe_api_category,
         name='recipe_api_v2_category',
     ),
+    path('recipes/api/v2/', include(recipe_api_v2_router.urls)),
 ]
+
+# urlpatterns += recipe_api_v2_router.urls
